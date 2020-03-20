7b7bd311342ecaeacf6c79b62cf4029c8459b7e0
H. Rüger <hannesrueger@gmx.de>
2020-03-20 08:56:54 -1000

    added first version

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,38 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as childProcess from 'child_process'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const osvar = process.platform.toLowerCase()
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    if (osvar === 'darwin') {
+      // MacOSX
+    } else if (osvar === 'win32') {
+      // Windows
+      exec('setx', [
+        'path',
+        "'%path%;c:\\Program Files (x86)\\Android\\android-sdk\\build-tools\\29.0.3'"
+      ])
+    } else {
+      // Linux
+    }
+    exec('npm', ['i', '-g', 'nativescript'])
+    exec('tns', ['doctor'])
   } catch (error) {
     core.setFailed(error.message)
   }
+}
+
+function exec(cmd: string, args: string[]): void {
+  console.log(`Executing command "${cmd}" with ${args.toString()}`)
+  const child = childProcess.spawnSync(cmd, args, {encoding: 'utf8'})
+  console.log('Process finished.')
+  if (child.error) {
+    console.log('ERROR: ', child.error)
+  }
+  console.log('stdout: ', child.stdout)
+  console.log('stderr: ', child.stderr)
+  console.log('exist code: ', child.status)
 }
 
 run()
